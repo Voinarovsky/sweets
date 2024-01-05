b1db2d3f31d2d36135ec4f2b5505a1326b53e804
kir <dzis-voinarovsky@yandex.com>
2024-01-06 02:28:30 +0300

    add file

--- dz3/6.py
+++ dz3/6.py
@@ -1,3 +1,5 @@
 import pandas as pd
+import matplotlib
 df = pd.read_csv('https://raw.githubusercontent.com/Voinarovsky/sweets/master/dz3/yandex_tracks_top100.csv')
-type(df)
+# df['track_len'].plot(kind='kde')
+print(df)
