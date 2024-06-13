3b2c246c0e50f3709dfebcd9973319fbd8026a58
kir <dzis-voinarovsky@yandex.com>
2024-06-14 02:36:53 +0300

    add file

--- Lab_Suir/laba9/app.py
+++ Lab_Suir/laba9/app.py
@@ -4,33 +4,27 @@ from flask_migrate import Migrate
 import sqlite3
 
 app = Flask('pomogite')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 
-class Feedback(dp.Model):
-    id = dp.Column(dp.Ineger, primary_key = True )
-    text = dp.Column(dp.String(500))
-    estimation = dp.Column(dp.Integer(5))
-    def __repr__(self):
-        return f'<Feedback {self.id} / {self.text}> {self.estimation}'
+# class Feedback(dp.Model):
+#     id = dp.Column(dp.Ineger, primary_key = True )
+#     text = dp.Column(dp.String(500))
+#     estimation = dp.Column(dp.Integer(5))
+#     def __repr__(self):
+#         return f'<Feedback {self.id} / {self.text}> {self.estimation}'
 
 feedbacks = [
     {'id': 8383,
      'response': 'плохо',
-     'estimation1': False,
-     'estimation2': False,
-     'estimation3': False},
+     'estimation': 1},
     {'id': 8353,
      'response': 'плохо',
-     'estimation1': False,
-     'estimation2': False,
-     'estimation3': False},
+     'estimation': 2},
     {'id': 84533,
      'response': 'плохо',
-     'estimation1': False,
-     'estimation2': False,
-     'estimation3': False}
+     'estimation': 3}
     ]
 
 @app.route('/')
@@ -40,31 +34,13 @@ def main():
     return render_template('main.html', feedbacks_list = feedbacks)
 
 
-@app.route('/estimation1/<int:feedback_id>', methods = ['PATCH'])
+@app.route('/estimation/<int:feedback_id>', methods = ['PATCH'])
 def modify_feedback1(feedback_id):
     global feedbacks
-    estimation1 = request.json['estimation1']
+    estimation = request.json['estimation']
     for feedback in feedbacks:
         if feedback['id'] == feedback_id:
-            feedback.update({'estimation1': estimation1})
-    return 'ok'
-
-@app.route('/estimation2/<int:feedback_id>', methods = ['PATCH'])
-def modify_feedback2(feedback_id):
-    global feedbacks
-    estimation2 = request.json['estimation2']
-    for feedback in feedbacks:
-        if feedback['id'] == feedback_id:
-            feedback.update({'estimation2': estimation2})
-    return 'ok'
-
-@app.route('/estimation3/<int:feedback_id>', methods = ['PATCH'])
-def modify_feedback3(feedback_id):
-    global feedbacks
-    estimation3 = request.json['estimation3']
-    for feedback in feedbacks:
-        if feedback['id'] == feedback_id:
-            feedback.update({'estimation3': estimation3})
+            feedback.update({'estimation': estimation})
     return 'ok'
 @app.route('/feedback', methods=['POST'])
 def create_feedback():
@@ -76,6 +52,6 @@ def create_feedback():
     return 'ok'
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     app.run(debug=True)
--- Lab_Suir/laba9/static/mainJS.js
+++ Lab_Suir/laba9/static/mainJS.js
@@ -1,37 +1,19 @@
-function updateFeedback1(el){
+function updateFeedback(el){
     feedback_id = el.value
-    fetch('/estimation1/' + feedback_id,
+    fetch('/estimation/' + feedback_id,
         {method: 'patch',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'estimation1': el.checked})
-        })
-}
-function updateFeedback2(el){
-    feedback_id = el.value
-    fetch('/estimation2/' + feedback_id,
-        {method: 'patch',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'estimation2': el.checked})
-        })
-}
-function updateFeedback3(el){
-    feedback_id = el.value
-    fetch('/estimation3/' + feedback_id,
-        {method: 'patch',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'estimation3': el.checked})
+        body: JSON.stringify({'estimation': el.checked})
         })
 }
 function createFeedback(){
     console.log('Create')
     let response = document.getElementById('response').value
-    let estimation1 = document.getElementById('estimation1').value
-    let estimation2 = document.getElementById('estimation2').value
-    let estimation3 = document.getElementById('estimation3').value
+    let estimation = document.getElementById('estimation').value
 
     fetch('/feedback',{
         method: 'post',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({'response': response || 'Без отзыва', 'estimation1': false, 'estimation2': false, 'estimation3': false })
+        body: JSON.stringify({'response': response || 'Без отзыва', 'estimation': estimation})
     })
 }
