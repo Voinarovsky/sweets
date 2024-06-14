d0e9b144b432fa86eff8a892b583ead94994f0f5
kir <dzis-voinarovsky@yandex.com>
2024-06-14 03:44:12 +0300

    add file

--- Lab_Suir/laba9/app.py
+++ Lab_Suir/laba9/app.py
@@ -4,54 +4,55 @@ from flask_migrate import Migrate
 import sqlite3
 
 app = Flask('pomogite')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# class Feedback(dp.Model):
-#     id = dp.Column(dp.Ineger, primary_key = True )
-#     text = dp.Column(dp.String(500))
-#     estimation = dp.Column(dp.Integer(5))
-#     def __repr__(self):
-#         return f'<Feedback {self.id} / {self.text}> {self.estimation}'
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key = True )
+    response = db.Column(db.String(500))
+    estimation = db.Column(db.Integer)
+    def __repr__(self):
+        return f'<Feedback {self.id} / {self.response} / {self.estimation}>'
 
-feedbacks = [
-    {'id': 8383,
-     'response': 'плохо',
-     'estimation': 1},
-    {'id': 8353,
-     'response': 'плохо',
-     'estimation': 2},
-    {'id': 84533,
-     'response': 'плохо',
-     'estimation': 3}
-    ]
+# feedbacks = [
+#     {'id': 8383,
+#      'response': 'плохо',
+#      'estimation': 1},
+#     {'id': 8353,
+#      'response': 'плохо',
+#      'estimation': 2},
+#     {'id': 84533,
+#      'response': 'плохо',
+#      'estimation': 3}
+#     ]
 
 @app.route('/')
 def main():
-    # feedbacks = Feedback.query.all()
-    # print(feedbacks)
+    feedbacks = Feedback.query.all()
+    print(feedbacks)
     return render_template('main.html', feedbacks_list = feedbacks)
 
 
-@app.route('/estimation/<int:feedback_id>', methods = ['PATCH'])
-def modify_feedback1(feedback_id):
-    global feedbacks
-    estimation = request.json['estimation']
-    for feedback in feedbacks:
-        if feedback['id'] == feedback_id:
-            feedback.update({'estimation': estimation})
-    return 'ok'
+# @app.route('/estimation/<int:feedback_id>', methods = ['PATCH'])
+# def modify_feedback(feedback_id):
+#     feedback = Feedback.query.get(feedback_id)
+#     feedback.estimation = request.json['estimation']
+#     db.session.commit()
+#     return 'ok'
 @app.route('/feedback', methods=['POST'])
 def create_feedback():
     data = request.json
-    last_id = feedbacks[-1]['id']
-    new_id = last_id + 1
-    data['id'] = new_id
-    feedbacks.append(data)
+    feedback = Feedback(**data)
+    db.session.add(feedback)
+    db.session.commit()
+    # last_id = feedbacks[-1]['id']
+    # new_id = last_id + 1
+    # data['id'] = new_id
+    # feedbacks.append(data)
     return 'ok'
 
 if __name__ == '__main__':
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
